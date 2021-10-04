c2547fc0eb9847efaabbbc47610558b6b4f3b4c6
cgarcia-UCO <42171969+cgarcia-UCO@users.noreply.github.com>
2021-10-04 23:56:48 +0200

    Update __init__.py

--- v002/__init__.py
+++ v002/__init__.py
@@ -5,7 +5,7 @@ try:
         from IPython import display
         i_am_in_interatcive = True
         import pylab as pl
-        pl.rcParams['figure.figsize'] = [25, 25]
+        pl.rcParams['figure.figsize'] = [10, 10]
         print("INTERACTIVE")
     else:
         import matplotlib.pyplot as pl
