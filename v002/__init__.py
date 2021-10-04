be782c1d4dfab1aff40ce8a2094318eff8c3abcd
cgarcia-UCO <42171969+cgarcia-UCO@users.noreply.github.com>
2021-10-04 22:49:21 +0200

    Update __init__.py

--- v002/__init__.py
+++ v002/__init__.py
@@ -1,16 +1,20 @@
 try:
     from IPython import get_ipython
 
-    if get_ipython().__class__.__name__ not in ['NoneType', 'Shell']:
+    if get_ipython().__class__.__name__ not in ['NoneType']:
         from IPython import display
         i_am_in_interatcive = True
         import pylab as pl
+        print("INTERACTIVE")
     else:
         import matplotlib.pyplot as pl
         i_am_in_interatcive = False
+        print("NOT INTERACTIVE")
 except:
     import matplotlib.pyplot as pl
     i_am_in_interatcive = False
+
+print("__INIT__ EXECUTED")
 
 from .Agent import Agent
 from .Enviroment_with_agents import Enviroment_with_agents
