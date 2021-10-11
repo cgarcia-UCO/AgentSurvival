b6275f15284bcee724730a2af87a9fa120b3a4d3
cgarcia-UCO <42171969+cgarcia-UCO@users.noreply.github.com>
2021-10-11 15:50:36 +0200

    Add files via upload

--- v002/InOut_Simple_Laberinth.py
+++ v002/InOut_Simple_Laberinth.py
@@ -99,7 +99,7 @@ class InOut_Simple_Laberinth(Enviroment_with_agents):
         return len(self._Enviroment_with_agents__living_agent_ids) <= 0 or self._exit_found #(self._epoch > 10 * self._size[0] * self._size[1]) or self._exit_found
 
     def create_agent(self, name, agent_class):
-        life = 10000
+        life = 10* self._size[0] * self._size[1]
         super().create_agent(name, agent_class,
                              self.entry._pos_y,
                              self.entry._pos_x,
